c2091c4b03a339ca62e6f64bb22f529f7f8469d8
Ville Haapavaara <ville.haapavaara@cs.tamk.fi>
2017-04-10 13:52:46 +0300

    Add fragments for new hike and hike list

---
+++ app/src/main/java/ville/fi/hikemate/Fragments/NewHikeFragment.java
@@ -0,0 +1,30 @@
+package ville.fi.hikemate.Fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ville.fi.hikemate.R;
+
+public class NewHikeFragment extends Fragment {
+
+    public NewHikeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_new_hike, container, false);
+    }
+}
