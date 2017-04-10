6b98e09cfec343854b728f7a5b2802f01503e170
Ville Haapavaara <ville.haapavaara@cs.tamk.fi>
2017-04-10 14:36:44 +0300

    Add a new activity for a map view

---
+++ app/src/main/java/ville/fi/hikemate/Activities/MapActivity.java
@@ -0,0 +1,15 @@
+package ville.fi.hikemate.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ville.fi.hikemate.R;
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
