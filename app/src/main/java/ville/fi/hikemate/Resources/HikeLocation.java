813d15b9badd22e3ec4323e2879725a33a401b69
Ville Haapavaara <ville.haapavaara@cs.tamk.fi>
2017-04-10 09:52:49 +0300

    Add basic features

---
+++ app/src/main/java/ville/fi/hikemate/Resources/HikeLocation.java
@@ -0,0 +1,32 @@
+package ville.fi.hikemate.Resources;
+
+/**
+ * Created by Ville on 10.4.2017.
+ */
+
+public class HikeLocation {
+
+    private double lat;
+    private double lng;
+
+    public HikeLocation(double lat, double lng) {
+        setLat(lat);
+        setLng(lng);
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLng() {
+        return lng;
+    }
+
+    public void setLng(double lng) {
+        this.lng = lng;
+    }
+}
