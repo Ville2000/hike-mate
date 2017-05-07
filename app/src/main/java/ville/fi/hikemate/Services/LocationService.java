2abeff2fb614c9c6c6184de394a47912a6a681f3
Ville Haapavaara <ville.haapavaara@cs.tamk.fi>
2017-05-07 13:34:34 +0300

    Refactor location tracking to be done in the service

--- app/src/main/java/ville/fi/hikemate/Services/LocationService.java
+++ app/src/main/java/ville/fi/hikemate/Services/LocationService.java
@@ -1,11 +1,25 @@
 package ville.fi.hikemate.Services;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.os.IBinder;
-import android.support.annotation.IntDef;
+import android.support.v4.content.LocalBroadcastManager;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.LinkedList;
 
 public class LocationService extends Service {
+
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private LinkedList<LatLng> locations;
+
     public LocationService() {
     }
 
@@ -17,6 +31,48 @@ public class LocationService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        locations = new LinkedList<>();
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        initLocationListener();
+
+        try {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 500, 1, locationListener);
+        } catch(SecurityException e) {
+            e.printStackTrace();
+        }
         return START_STICKY;
+    }
+
+    private void sendGPSLocations(double latitude, double longitude) {
+        Intent i = new Intent("GPSLocations");
+        locations.add(new LatLng(latitude, longitude));
+        i.putExtra("Locations", locations);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(i);
+    }
+
+    private void initLocationListener() {
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                sendGPSLocations(location.getLatitude(), location.getLongitude());
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            @Override
+            public void onProviderEnabled(String provider) {}
+
+            @Override
+            public void onProviderDisabled(String provider) {}
+        };
+    }
+
+    @Override
+    public boolean stopService(Intent name) {
+        System.out.println("Stopping LocationService");
+        locationManager = null;
+        locationListener = null;
+        return super.stopService(name);
     }
 }
