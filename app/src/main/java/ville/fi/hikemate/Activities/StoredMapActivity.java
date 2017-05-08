ed96790c8dc829f62b5d35824be816dbea6c367b
Ville Haapavaara <ville.haapavaara@cs.tamk.fi>
2017-05-08 19:41:47 +0300

    Add photo markers to be viewed in the map and add a click listner to them

--- app/src/main/java/ville/fi/hikemate/Activities/StoredMapActivity.java
+++ app/src/main/java/ville/fi/hikemate/Activities/StoredMapActivity.java
@@ -3,12 +3,15 @@ package ville.fi.hikemate.Activities;
 import android.content.Context;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 
@@ -17,6 +20,8 @@ import java.util.LinkedList;
 import ville.fi.hikemate.R;
 import ville.fi.hikemate.Resources.Hike;
 import ville.fi.hikemate.Resources.HikeLocation;
+import ville.fi.hikemate.Resources.PhotoMapMarker;
+import ville.fi.hikemate.Utils.Debug;
 import ville.fi.hikemate.Utils.HikeToLatLng;
 import ville.fi.hikemate.Utils.StorageHandler;
 
@@ -26,6 +31,7 @@ public class StoredMapActivity extends FragmentActivity implements OnMapReadyCal
     private GoogleMap mMap;
     private LinkedList<Hike> hikes;
     private LinkedList<LatLng> hike;
+    private LinkedList<PhotoMapMarker> photoMapMarkers;
     private StorageHandler sh;
     private Polyline hikePolyLine;
 
@@ -47,25 +53,38 @@ public class StoredMapActivity extends FragmentActivity implements OnMapReadyCal
 
         hikes = sh.readStorage(host);
         hike = HikeToLatLng.getLatLng(hikes.get((int) getIntent().getExtras().get("position")));
+        photoMapMarkers = hikes.get((int) getIntent().getExtras().get("position")).getPhotoMapMarkers();
+        Debug.toastThis(this, hike.toString());
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+
         hikePolyLine = mMap.addPolyline(new PolylineOptions()
                 .clickable(false));
         hikePolyLine.setPoints(hike);
+
+        if (photoMapMarkers != null) {
+            for (PhotoMapMarker p : photoMapMarkers) {
+                googleMap.addMarker(new MarkerOptions().position(new LatLng(p.getLat(), p.getLon())));
+            }
+        }
+
+        initMarkerClickListener(googleMap);
+
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(hike.get(0).latitude, hike.get(0).longitude), 15));
     }
 
-    public LinkedList<LatLng> getLatLng(Hike hike) {
-        LinkedList<LatLng> locs = new LinkedList<>();
-
-        for (HikeLocation loc: hike.getLocations()) {
-            locs.add(new LatLng(loc.getLat(), loc.getLng()));
-        }
-
-        return locs;
+    private void initMarkerClickListener(GoogleMap map) {
+        map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                Debug.toastThis(host, "Clicking Markers");
+                marker.showInfoWindow();
+                return false;
+            }
+        });
     }
 }
