5cdfe3d8c9773d3b55b4e6ec1413cb959aa40cca
Ville Haapavaara <ville.haapavaara@cs.tamk.fi>
2017-05-08 19:44:49 +0300

    Add a photo marker class to save location and photo path of the user taken photo

---
+++ app/src/main/java/ville/fi/hikemate/Resources/PhotoMapMarker.java
@@ -0,0 +1,56 @@
+package ville.fi.hikemate.Resources;
+
+import com.google.android.gms.maps.model.Marker;
+
+/**
+ * Created by Ville on 8.5.2017.
+ */
+
+public class PhotoMapMarker {
+
+    private String path;
+    private double lat;
+    private double lon;
+
+    public PhotoMapMarker() {
+
+    }
+
+    public PhotoMapMarker(String path, double latitude, double longitude) {
+        setPath(path);
+        setLat(latitude);
+        setLon(longitude);
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+
+    public String toString() {
+        String str = "";
+
+        str += "{";
+
+        return "{ \"path\": " + path + ", \"lat\": " + String.valueOf(lat) + ", \"lng\": " + String.valueOf(lon) + " }";
+    }
+}
